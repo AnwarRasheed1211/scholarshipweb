f06b20be1de318ebd989e087b81e37fcc9911815
Anwar <darkninja1211@hotmail.com>
2023-09-08 23:41:11 +0700

    updated

---
+++ scholarshipweb/components/StaffNavbar.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './home.module.css';
+import Image from 'next/image';
+
+const StaffNavbar = () => {
+  const router = useRouter();
+
+  if (router.pathname === '/staffHome') {
+    return (
+      <nav className={styles.navBar}>
+        <div className={styles.logo}>
+          <div className={styles.logoContainer}>
+            <Image src="/abac_logo.png" alt="Logo" width={40} height={40} />
+              <div className={styles.logoText}>
+                AU Scholarship
+              </div>
+          </div>
+          <div className={styles['imge-container']}>
+            <Link href={'/'}>
+            <div className={styles['text2']}>Log Out</div>
+            </Link>
+            <Image src="/work_upload.png" alt="Another Image" width={35} height={35} />
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+  return null;
+};
+
+export default StaffNavbar;
